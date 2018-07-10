6c0bef92a50c19bf82e6e767856f60faa4dd7df5
wiilee <willy.cai@icloud.com>
2018-07-10 19:40:44 +0200

    integrated all functions into a class

--- code/project.py
+++ code/project.py
@@ -1,68 +1,77 @@
-import sys
-from naoqi import ALProxy
-
 '''
 USAGE: type in terminal: python project.py "10.0.7.1X"
     X is the number of the robot
 '''
 
 
-# different motion listed here
-def nao_motion(state, ip, port):
-
-    try:
-        motion = ALProxy("ALMotion", ip, port)
-    except Exception, e:
-        print "Could not create proxy to ALRobotPosture"
-        print "Error was: ", e
-
-    if state == 'wakeUp':
-        motion.wakeUp()
-    elif state == 'rest':
-        motion.rest()
+import sys
+from naoqi import ALProxy
 
 
-# simple example for speaking
-def nao_speak(text, ip, port):
+class AssistentAgent:
 
-    try:
-        tts = ALProxy("ALTextToSpeech", ip, port)
-    except Exception, e:
-        print "Could not create proxy to ALTextToSpeech"
-        print "Error was: ", e
+    port = 9559
 
-    tts.say(text)
-
-# simple example for build-in posture
-def nao_posture(posture, ip, port):
-
-    try:
-        postureProxy = ALProxy("ALRobotPosture", ip, port)
-    except Exception, e:
-        print "Could not create proxy to ALRobotPosture"
-        print "Error was: ", e
-
-    postureProxy.goToPosture(posture, 1.0)
+    def __init__(self, ip):
+        self.ip = ip
 
 
+    # simple example for motion
+    def motion(self, state):
 
-def main():
+        try:
+            motion = ALProxy("ALMotion", self.ip, self.port)
+        except Exception, e:
+            print "Could not create proxy to ALRobotPosture"
+            print "Error was: ", e
 
+        if state == 'wakeUp':
+            motion.wakeUp()
+        elif state == 'rest':
+            motion.rest()
+
+
+    # simple example for speaking
+    def speak(self, text):
+
+        try:
+            tts = ALProxy("ALTextToSpeech", self.ip, self.port)
+        except Exception, e:
+            print "Could not create proxy to ALTextToSpeech"
+            print "Error was: ", e
+
+        tts.say(text)
+
+
+    # simple example for build-in posture
+    def posture(self, posture):
+
+        try:
+            postureProxy = ALProxy("ALRobotPosture", self.ip, self.port)
+        except Exception, e:
+            print "Could not create proxy to ALRobotPosture"
+            print "Error was: ", e
+
+        postureProxy.goToPosture(posture, 1.0)
+
+
+if __name__ =='__main__':
     # parameter for naobot
-    nao_ip = sys.argv[1]
-    nao_port = 9559
+    try:
+        nao_ip = sys.argv[1]
+    except Exception, e:
+        print "Invalid IP address! Usage: python project.py <IP address>"
+        print "Error was: ", e
+
+    nao = AssistentAgent(nao_ip)
 
     # before u ran your code, NaoBot needs to wake up!
-    nao_motion('wakeUp', nao_ip, nao_port)
+    nao.motion('wakeUp')
 
     # enter your code here down below
 
-    #nao_speak("mama", nao_ip, nao_port)
-    #nao_posture("StandInit", nao_ip, nao_port)
-    #nao_motion(nao_ip, nao_port)
+    #nao.speak("mama")
+    #nao.posture("StandInit")
 
     # after u ran your code, let Naobot rest!
-    nao_motion('rest', nao_ip, nao_port)
-
-if __name__ =='__main__':
-    main()
+    nao.motion('rest', nao_ip, nao_port)
