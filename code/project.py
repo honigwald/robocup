c3370af366bc8eb2af1ff62b77f46b472b55f3a8
honigwald <honigwald@icloud.com>
2018-11-05 18:51:31 +0100

    implemented facetracking and facerecognition. needs a lot of testing now

--- code/project.py
+++ code/project.py
@@ -30,6 +30,7 @@ class AssistentAgent:
                 print e
                 return 'what do you mean? i do not understand'
 
+    ### Controlling facerecognition API
     def face(self, state):
         try:
             faceProxy = ALProxy("ALFaceDetection", self.ip, self.port)
@@ -40,126 +41,98 @@ class AssistentAgent:
         if state == 'learn':
             self.say('Tell me your name')
             name = self.speech_recognize(1.0).lower()
-
-        elif state == 'cleardb':
-        elif state == 'identify':
-
-
-
-    #def learn_face(self, name):
-    def flearn(self, name):
-        try:
-            faceProxy = ALProxy("ALFaceDetection", self.ip, self.port)
-        except Exception, e:
-            print "Error when creating face detection proxy:"
-            print str(e)
-
-        #print "Actual DB: %s" % (faceProxy.getLearnedFacesList()) 
-        if (faceProxy.learnFace(name)):
-            print "Learning is complete"
-        else:
-            print "Something went wrong"
-
-    #def clear_facedb(self):
-    def fcleardb(self):
-        try:
-            faceProxy = ALProxy("ALFaceDetection", self.ip, self.port)
-        except Exception, e:
-            print "Error when creating face detection proxy:"
-            print str(e) 
-
-        #print "Actual DB: %s" % (faceProxy.getLearnedFacesList()) 
-        if (faceProxy.clearDatabase()):
-            print "DB cleared"
-        else:
-            print "Something went wrong"
-
-    def fidentify(self):
-        try:
-            faceProxy = ALProxy("ALFaceDetection", self.ip, self.port)
-        except Exception, e:
-            print "Error when creating face detection proxy:"
-            print str(e) 
-
-        period = 500
-        faceProxy.subscribe("Test_Face", period, 0.0 )
-
-        # ALMemory variable where the ALFaceDetection module
-        # outputs its results.
-        memValue = "FaceDetected"
-
-        # Create a proxy to ALMemory
-        try:
-            memoryProxy = ALProxy("ALMemory", IP, PORT)
-        except Exception, e:
-            print "Error when creating memory proxy:"
-            print str(e)
-            exit(1)
-
-        val = memoryProxy.getData(memValue, 0)
-        if(val and isinstance(val, list) and len(val) == 5):
-            # We detected faces !
-            # For each face, we can read its shape info and ID.
-            # First Field = TimeStamp.
-            timeStamp = val[0]
-            # Second Field = array of face_Info's.
-            faceInfoArray = val[1]
-
-            ### THIS IS IMPORTANT
-            ### DETERMINING THE NAME OF RECOGNIZED PERSON
-            print "Name of recognized Person: %s" % val[1][0][1][2]
-            name = val[1][0][1][2]
-            if name != '':
-                name = "unknown"
-        else:
-            print "Error with getData. ALValue = %s" % (str(val))
-            # Unsubscribe the module.
-
-        faceProxy.unsubscribe("Test_Face")
-        return name
-
-
-    def rundemo(self):
-        #TODO
-        print "Demo is running"
-        self.motion('wakeUp')
-        self.tracker('start')
-
-        for i in 20:
-            self.tracker('check')
-            time.sleep(1)
-        self.tracker('stop')
-
-        self.motion('rest')
-        print "Demo is finished"
-
-    def tracker(self, state):
-        try:
-            tracker = ALProxy("ALTracker", self.ip, self.port)
-        except Exception, e:
-            print "Could not create proxy to ALTracker!"
-            print "Error was: ", e
-
-        if state == "start":
-            targetName = "Face"
-            faceWidth = 0.1
-            tracker.setMode('Head')
-            tracker.registerTarget(targetName, faceWidth)
-
-            # Then, start tracker.
-            tracker.track(targetName)
-
-        elif state == "check":
-            if tracker.isNewTargetDetected():
-                print "new target detected"
+            if (faceProxy.learnFace(name)):
+                print "Learning is complete"
+                self.say('Thank you')
             else:
-                print "no target"
+                print "Something went wrong"
+        elif state == 'cleardb':
+            if (faceProxy.clearDatabase()):
+                print "DB cleared"
+                self.say('Database cleared')
+                else:
+                    print "Something went wrong"
+            elif state == 'identify':
+                period = 500
+                faceProxy.subscribe("Test_Face", period, 0.0 )
+                memValue = "FaceDetected"
+                try:
+                    memoryProxy = ALProxy("ALMemory", self.ip, self.port)
+                except Exception, e:
+                    print "Error when creating memory proxy:"
+                    print str(e)
+                    exit(1)
 
-        elif state == "stop":
-            tracker.stopTracker()
-            tracker.unregisterAllTargets()
+                val = memoryProxy.getData(memValue, 0)
+                if(val and isinstance(val, list) and len(val) == 5):
+                    # We detected faces !
+                    # For each face, we can read its shape info and ID.
+                    # First Field = TimeStamp.
+                    timeStamp = val[0]
+                    # Second Field = array of face_Info's.
+                    faceInfoArray = val[1]
 
-    def move(self, X, Y):
+                    ### THIS IS IMPORTANT
+                    ### DETERMINING THE NAME OF RECOGNIZED PERSON
+                    print "Name of recognized Person: %s" % val[1][0][1][2]
+                    name = val[1][0][1][2]
+                    if name != '':
+                        name = "unknown"
+                else:
+                    print "Error with getData. ALValue = %s" % (str(val))
+            
+                # Unsubscribe the module.
+                faceProxy.unsubscribe("Test_Face")
+                return name
+        
+        ### The final demonstration of the project
+        def rundemo(self):
+            #TODO: Actually it's just some small testing here
+            print "Demo is running"
+            self.motion('wakeUp')
+            self.tracker('start')
+
+            for i in 20:
+                self.tracker('check')
+                name = self.face('identify')
+                if name != 'unknown':
+                    self.say('Hello')
+                    self.say(name)
+                time.sleep(1)
+            self.tracker('stop')
+
+            self.motion('rest')
+            print "Demo is finished"
+        
+        ### Controlling the facetracking function of nao
+        def tracker(self, state):
+            try:
+                tracker = ALProxy("ALTracker", self.ip, self.port)
+            except Exception, e:
+                print "Could not create proxy to ALTracker!"
+                print "Error was: ", e
+
+            if state == "start":
+                targetName = "Face"
+                faceWidth = 0.1
+                tracker.setMode('Head')
+                tracker.registerTarget(targetName, faceWidth)
+
+                # Then, start tracker.
+                tracker.track(targetName)
+
+            elif state == "check":
+                if tracker.isNewTargetDetected():
+                    print "new target detected"
+                else:
+                    print "no target"
+
+            elif state == "stop":
+                tracker.stopTracker()
+                tracker.unregisterAllTargets()
+
+        ### Controlling the movement of nao 
+        def move(self, X, Y):
         try:
             motionProxy  = ALProxy("ALMotion", self.ip, self.port)
             postureProxy = ALProxy("ALRobotPosture", self.ip, self.port)
@@ -187,7 +160,7 @@ class AssistentAgent:
 
         return 0
 
-
+    ### Nao speaks given text
     def say(self, text):
         try:
             tts = ALProxy("ALTextToSpeech", self.ip, self.port)
@@ -323,9 +296,7 @@ if __name__ =='__main__':
                         nao.say('say ok when you are ready')
                         resp = nao.speech_recognize(2.0).lower()
                         if resp == 'ok':
-                            nao.say('tell me your name')
-                            name = nao.speech_recognize(1.0).lower()
-                            nao.flearn(name)
+                            nao.face('learn')
                         else:
                             nao.say('oh oh something went wrong')
 
@@ -336,7 +307,7 @@ if __name__ =='__main__':
                         nao.say('say ok if you really want that')
                         resp = nao.speech_recognize(2.0).lower()
                         if resp == 'ok':
-                            nao.fcleardb(name)
+                            nao.face('cleardb')
                             nao.say('database cleared')
                         else:
                             nao.say('oh oh something went wrong')
